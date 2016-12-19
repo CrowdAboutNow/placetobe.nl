304eeec11ece2110b86f25bb96580c26be0a1afd
Wouter Vroege <woutervroege@MacBookProRetinaWV.fritz.box>
2016-12-19 11:13:21 +0100

    fixing build

--- server.js
+++ server.js
@@ -3,16 +3,37 @@ var express = require('express'),
   fs = require('fs'),
   handlebars = require('handlebars'),
   async = require('async'),
+  request = require('request-json'),
+  client = request.createClient('https://crowdaboutnow.nl/'),
   metadata = require('./metadata.json');
 
 app.use(express.static(__dirname));
 var index = fs.readFileSync('index.hbs').toString();
 var template = handlebars.compile(index);
 
+
+/*
+Campaign Page
+*/
+app.get('/campagnes/:slug', (req, res) => {
+  console.log('req', req.params.slug)
+  client.get('api/campaigns/' + req.params.slug, function(error, response, data) {
+    if(!data) return res.sendStatus(404);
+    var data = {
+      "title": data.projectNaam,
+      "description": data.introductieTekst,
+      "imageUrl": data.coverURL
+    }
+    var html = template(data);
+    res.send(html);
+  })
+})
+
+/*
+Static Pages
+*/
 app.get('*', (req, res) => {
-
   var data = metadata.routes[req.url];
-
   if(!data) return res.sendStatus(404);
   var html = template(data);
   res.send(html);
